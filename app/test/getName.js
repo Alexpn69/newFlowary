620b1e3971b12305f05df20aee1a04f202e39d20
Alex <alexpvn69@gmail.com>
2023-06-21 22:43:21 +0300

    fixBug

--- app/test/getName.js
+++ app/test/getName.js
@@ -1,23 +1,23 @@
-"use client";
+// "use client";
 
-import getRecordTest from "@/logic/serverFunc/getRecordTest";
-import postRecordTest from "@/logic/serverFunc/postRecordTest";
+// import getRecordTest from "@/logic/serverFunc/getRecordTest";
+// import postRecordTest from "@/logic/serverFunc/postRecordTest";
 
-const GetName = () => {
-  return (
-    <>
-      <form onSubmit={getRecordTest}>
-        <input type="text" required />
-        <button>GET</button>
-      </form>
+// const GetName = () => {
+//   return (
+//     <>
+//       <form onSubmit={getRecordTest}>
+//         <input type="text" required />
+//         <button>GET</button>
+//       </form>
 
-      <form onSubmit={postRecordTest}>
-        <input type="text" required />
-        <input type="text" required />
-        <button>POST</button>
-      </form>
-    </>
-  );
-};
+//       <form onSubmit={postRecordTest}>
+//         <input type="text" required />
+//         <input type="text" required />
+//         <button>POST</button>
+//       </form>
+//     </>
+//   );
+// };
 
-export default GetName;
+// export default GetName;
